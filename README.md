d068e5015c9358b02134b93a07f0150e19dcaa48
changyue <zhangrd@tuya.com>
2020-04-01 17:34:36 +0800

    feat: upgrade demo

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ## Android IoT SDK
 
+### demo使用
+demo 提供了获取激活码、激活、dp点测试、状态日志展示等功能。
+> **注意** 激活需要的pid、uuid、authkey需要在本地配置中填写,
+> 在你的`local.properties`文件中增加如下:  
+> PRODUCT_ID=你的pid  
+> UUID=你的uuid  
+> AUTHKEY=你的key
+
+![demo截图](./demo_screenshot.jpeg)
+
 ### 接入
 
 * 混淆  
--- app/src/main/java/com/tuya/smartai/demo/MainActivity.java
+++ app/src/main/java/com/tuya/smartai/demo/MainActivity.java
@@ -8,20 +8,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alibaba.fastjson.JSONObject;
 import com.tuya.smartai.iot_sdk.DPEvent;
-import com.tuya.smartai.iot_sdk.HttpResponse;
 import com.tuya.smartai.iot_sdk.IoTSDKManager;
 
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Properties;
 
-import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
@@ -38,20 +42,52 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
     IoTSDKManager ioTSDKManager;
 
+    ImageView qrCode;
+    TextView shortUrl;
+    TextView console;
+
+    Switch boolView;
+    EditText intView;
+    RadioGroup enumView;
+    EditText strView;
+    EditText rawView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.reset).setOnClickListener(this::onClick);
-        findViewById(R.id.dp_send).setOnClickListener(this::onClick);
-        findViewById(R.id.http).setOnClickListener(this::onClick);
+        qrCode = findViewById(R.id.qrcode);
+        console = findViewById(R.id.console);
+        console.setOnLongClickListener(v -> {
+            clear();
+            return false;
+        });
+        shortUrl = findViewById(R.id.shorturl);
+
+        initDPViews();
 
         if (!EasyPermissions.hasPermissions(this, requiredPermissions)) {
             EasyPermissions.requestPermissions(this, "需要授予权限以使用设备", PERMISSION_CODE, requiredPermissions);
         } else {
             initSDK();
         }
+    }
+
+    private void initDPViews() {
+
+        findViewById(R.id.bool_send).setOnClickListener(this::onClick);
+        findViewById(R.id.int_send).setOnClickListener(this::onClick);
+        findViewById(R.id.enum_send).setOnClickListener(this::onClick);
+        findViewById(R.id.string_send).setOnClickListener(this::onClick);
+        findViewById(R.id.raw_send).setOnClickListener(this::onClick);
+
+        boolView = findViewById(R.id.bool_val);
+        intView = findViewById(R.id.int_val);
+        enumView = findViewById(R.id.enum_val);
+        strView = findViewById(R.id.string_val);
+        rawView = findViewById(R.id.raw_val);
     }
 
     private void initSDK() {
@@ -64,13 +100,14 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
             }
         };
 
-        ioTSDKManager.initSDK("/sdcard/", "你的pid"
-                , "你的uuid", "你的authkey", new IoTSDKManager.IoTCallback() {
+        //注意：这里的pid等配置读取自local.properties文件，不能直接使用。请填写你自己的配置！
+        ioTSDKManager.initSDK("/sdcard/", BuildConfig.PRODUCT_ID
+                , BuildConfig.UUID, BuildConfig.AUTHOR_KEY, new IoTSDKManager.IoTCallback() {
 
                     @Override
                     public void onDpEvent(DPEvent event) {
                         if (event != null) {
-                            Log.d(TAG, "rev dp: " + event);
+                            output("收到 dp: " + event);
                         }
                     }
 
@@ -88,23 +125,34 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
                     }
 
                     @Override
-                    public void onShorturl(String url) {
-                        Log.w(TAG, "shorturl: " + url);
+                    public void onShorturl(String urlJson) {
+                        output("shorturl: " + urlJson);
+
+                        String url = (String) JSONObject.parseObject(urlJson).get("shortUrl");
+
+                        runOnUiThread(() -> {
+                            shortUrl.setText("用涂鸦智能APP扫码激活");
+                            qrCode.setImageBitmap(QRCodeUtil.createQRCodeBitmap(url, 400, 400));
+                        });
                     }
 
                     @Override
                     public void onActive() {
-                        Log.d(TAG, "onActive: " + ioTSDKManager.getDeviceId());
+                        output("onActive: devId-> " + ioTSDKManager.getDeviceId());
+
+                        runOnUiThread(() -> {
+                            shortUrl.setText("激活成功了");
+                        });
                     }
 
                     @Override
                     public void onFirstActive() {
-                        Log.d(TAG, "onFirstActive");
+                        output("onFirstActive");
                     }
 
                     @Override
                     public void onMQTTStatusChanged(int status) {
-                        Log.d(TAG, "onMQTTStatusChanged: " + status);
+                        output("onMQTTStatusChanged: " + status);
 
                         switch (status) {
                             case IoTSDKManager.STATUS_OFFLINE:
@@ -146,26 +194,61 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
             case R.id.reset:
                 ioTSDKManager.reset();
                 break;
-            case R.id.http:
-                JSONObject params = new JSONObject();
-                params.put("uuid", "1234");
-                params.put("appId", "10000");
-
-                if (httpdisposable != null) {
-                    httpdisposable.dispose();
-                }
-                httpdisposable = Observable.just(0)
-                        .subscribeOn(Schedulers.io())
-                        .subscribe(integer -> {
-                            HttpResponse response = ioTSDKManager.httpRequest("tuya.device.qrcode.info.get", "1.0", params.toJSONString());
-                            Log.w(TAG, response.toString());
-                        });
-
+//            case R.id.http:
+//                JSONObject params = new JSONObject();
+//                params.put("uuid", "1234");
+//                params.put("appId", "10000");
+//
+//                if (httpdisposable != null) {
+//                    httpdisposable.dispose();
+//                }
+//                httpdisposable = Observable.just(0)
+//                        .subscribeOn(Schedulers.io())
+//                        .subscribe(integer -> {
+//                            HttpResponse response = ioTSDKManager.httpRequest("tuya.device.qrcode.info.get", "1.0", params.toJSONString());
+//                            Log.w(TAG, response.toString());
+//                        });
+//
+//                break;
+            case R.id.bool_send:
+                ioTSDKManager.sendDP(101, DPEvent.Type.PROP_BOOL, boolView.isChecked());
                 break;
-            case R.id.dp_send:
-                ioTSDKManager.sendDP(1, DPEvent.Type.PROP_BOOL, false);
+            case R.id.int_send:
+                int intVal = Integer.parseInt(intView.getText().toString());
+                ioTSDKManager.sendDP(102, DPEvent.Type.PROP_VALUE, intVal);
+                break;
+            case R.id.string_send:
+                ioTSDKManager.sendDP(104, DPEvent.Type.PROP_STR, strView.getText().toString());
+                break;
+            case R.id.enum_send:
+                int checked = 0;
+                int radioButtonId = enumView.getCheckedRadioButtonId();
+                switch (radioButtonId) {
+                    case R.id.enum_0:
+                        checked = 0;
+                        break;
+                    case R.id.enum_1:
+                        checked = 1;
+                        break;
+                    case R.id.enum_2:
+                        checked = 2;
+                        break;
+                }
+                ioTSDKManager.sendDP(103, DPEvent.Type.PROP_ENUM, checked);
+                break;
+            case R.id.raw_send:
+                ioTSDKManager.sendDP(105, DPEvent.Type.PROP_RAW, rawView.getText().toString().getBytes(Charset.forName("UTF-8")));
                 break;
         }
+    }
+
+    private void output(String text) {
+        runOnUiThread(() -> console.append(text + "\n"));
+        Log.d(TAG, text);
+    }
+
+    private void clear() {
+        runOnUiThread(() -> console.setText("接收日志在这里输出(长按清除): \n"));
     }
 
     @Override
